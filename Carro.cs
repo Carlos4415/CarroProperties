7e7cbc5fa8665acb4d223253be9e249d807788a9
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:35:27 -0300

    first commit

---
+++ Carro.cs
@@ -0,0 +1,53 @@
+﻿class Carro
+{
+    private int ano;
+    public string Fabricante { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { 
+        get => ano; 
+        set
+        {
+            if (value < 1960 || value > 2025)
+            {
+                Console.WriteLine("Valor inválido, insira um ano entre 1960 e 2023");
+            }
+            else
+            {
+                ano = value;
+            }
+        }
+    }
+    public int QuantidadePortas { get; set; }
+    public int Velocidade { get; set; } = 0;
+    public string DescricaoDetalhada => $"Modelo do carro: {this.Fabricante} {this.Modelo} {this.Ano}";
+
+    public void exibirInformacoes()
+    {
+        Console.WriteLine($"\nInformações do carro: {this.Fabricante} {this.Modelo}, {this.QuantidadePortas} portas, {this.Ano}");
+    }
+
+    public void acelerar()
+    {
+        Console.WriteLine("\nAcelerando...");
+
+        if (Velocidade < 100)
+        {
+            Velocidade = Velocidade + 5;
+        }
+    }
+
+    public void frear()
+    {
+        Console.WriteLine("\nFreando...");
+
+        if (Velocidade > 0)
+        {
+            Velocidade = Velocidade - 5;
+        }
+    }
+
+    public void buzinar()
+    {
+        Console.WriteLine("\nBi Bi");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Este projeto implementa uma classe que modela um carro.  
+// A classe possui os seguintes atributos e métodos:  
+// - Atributos: podem incluir modelo, marca, ano, cor e velocidade atual.  
+// - Métodos:  
+//   - Acelerar: aumenta a velocidade do carro.  
+//   - Frear: reduz a velocidade do carro.  
+//   - Buzinar: emite um som característico de buzina.  
+// O objetivo é simular o comportamento básico de um carro, permitindo interações simples como acelerar, frear e buzinar.  
+Carro carro = new Carro();
+carro.Fabricante = "Honda";
+carro.Modelo = "Civic";
+carro.Ano = 2020;
+carro.QuantidadePortas = 4;
+
+carro.exibirInformacoes();
+Console.WriteLine($"\n{carro.DescricaoDetalhada}");
+carro.acelerar();
+Console.WriteLine($"\nVelocidade: {carro.Velocidade}");
+carro.frear();
+Console.WriteLine($"\nVelocidade: {carro.Velocidade}");
+carro.buzinar();
